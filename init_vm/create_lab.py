dc636308c0bf5212e71df9d66c51d348f4caa67d
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-11-21 13:15:40 -0600

    ask if input is correct

--- init_vm/create_lab.py
+++ init_vm/create_lab.py
@@ -20,23 +20,33 @@ def run_terraform_command(working_dir, command, variables):
         print(result.stdout)
 
 
-lab_name = '/lab'
+def gather_user_input():
 
-print("Enter the following details: ")
-VPC_NETWORK_NAME = str(input("VPC Network Name: "))
-SUBNET_NAME = str(input("Subnet Name: "))
-VM_INSTANCE_NAME = str(input("VM Name: "))
+    # lab_name = '/lab'
+    print("Enter the following details: ")
+    VPC_NETWORK_NAME = str(input("VPC Network Name: "))
+    SUBNET_NAME = str(input("Subnet Name: "))
+    VM_INSTANCE_NAME = str(input("VM Name: "))
 
-user_base_dir = './user'
-init_dir = './init_vm'
+    user_base_dir = './user'
+    init_dir = './init_vm'
 
-terraform_variables = {
-    'vpc_network_name': VPC_NETWORK_NAME,
-    'subnet_name': SUBNET_NAME,
-    'instance_name': VM_INSTANCE_NAME
-}
+    terraform_variables = {
+        'vpc_network_name': VPC_NETWORK_NAME,
+        'subnet_name': SUBNET_NAME,
+        'instance_name': VM_INSTANCE_NAME
+    }
+
+    print("Confirmed details: ")
+    print("VPC Network Name: " + VPC_NETWORK_NAME)
+    print("Subnet Name: " + SUBNET_NAME)
+    print("VM Name: " + VM_INSTANCE_NAME)
+    input = str(input("Is this correct? (y/n): "))
+
+    if (input == "y" or input == "Y" or input == "yes" or input == "Yes" or input == "YES"):
+        run_terraform_command(init_dir, 'init', {})
+        run_terraform_command(init_dir, 'apply', terraform_variables)
 
 
-run_terraform_command(init_dir, 'init', {})
-run_terraform_command(init_dir, 'apply', terraform_variables)
+gather_user_input()
 exit(1)
