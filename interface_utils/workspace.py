52e8053f66366e4cf7d04460f805773e1345540e
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-11-25 14:28:12 -0600

    made basic class for a workspace

--- interface_utils/workspace.py
+++ interface_utils/workspace.py
@@ -1,2 +1,55 @@
+import subprocess
+
+
 class Workspace:
+    terraform_dir = '././terraform_config'
+
+    def __init__(self, name):
+        self.name = name
+        self.lab_vm = None
+        self.lab_running = False
+        self.create_workspace(self)
+
+    def create_workspace(self):
+
+        command = ['workspace', 'new', self.name]
+        self.run_terraform_command(self, command, {})
+
+    def select_workspace(self):
+        command = ['workspace', 'select', self.name]
+        self.run_terraform_command(self, command, {})
+
+    def initialise_workspace(self):
+        command = ['init']
+        self.run_terraform_command(self, command, {})
+
+    def apply_workspace(self, variables):
+        command = ['apply']
+        self.run_terraform_command(self, command, variables)
+        self.lab_running = True
+
+    def run_terraform_command(self, command, variables):
+
+        working_dir = self.terraform_dir
+        # args = ["terraform", command]
+        args = ["terraform"]
+
+        if isinstance(command, list):
+            args.extend(command)
+        elif isinstance(command, str):
+            args.append(command)
+
+        if (command == 'apply'):
+            args.append('-auto-approve')
+
+        for key, value in variables.items():
+            args.extend(['-var', '{}={}'.format(key, value)])
+
+        # print(args)
+        result = subprocess.run(args, cwd=working_dir, text=True)
+
+        if result.returncode != 0:
+            print(result.stderr)
+        else:
+            print(result.stdout)
     pass
--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import shutil
 from os import name, system
 import os
 import sys
+from interface_utils.workspace import Workspace
+
+
 # current_dir = os.getcwd()
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -16,8 +19,17 @@ if (len(sys.argv) < 2):
     print("Please provide a command")
     exit(1)
 
+
 if (sys.argv[1] == "start"):
-    subprocess.run([sys.executable, "./interface_utils/create_lab_vm.py"])
+    if (len(sys.argv) < 3):
+        print("Please provide a workspace name")
+        exit(1)
+
+    workspace_name = sys.argv[2]
+    workspace = Workspace(sys.argv[2])
+
+    # print(workspace_name)
+    # subprocess.run([sys.executable, "./interface_utils/create_lab_vm.py"])
 elif (sys.argv[1] == "delete"):
     subprocess.run([sys.executable, "./interface_utils/destroy_lab_vm.py"])
 else:
